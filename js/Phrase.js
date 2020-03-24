652fb3966cab45fee9686190b612838c92d82104
Dominic Meyer <dominic.meyer@me.com>
2020-03-24 19:30:08 +0100

    Phrase Class - addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * adds letter placeholders to the display when the game starts
+     * @param   phrase the phrase who the player has to guess
+     */
+    addPhraseToDisplay(phrase) {
+        for(let i = 0; i < phrase.length; i++) {
+            let phraseChar = phrase[i];
+            let charLi;
+            if(phraseChar === " ") {
+                charLi = `<li class="space"> </li>`;
+            } else {
+                charLi = `<li class="hide letter ${phraseChar}">${phraseChar}</li>`;
+            }
+            $('#phrase ul').append(charLi);
+        }
+    }
+
+    checkLetter() {
+
+    }
+
+    showMatchedLetter() {
+
+    }
+}
