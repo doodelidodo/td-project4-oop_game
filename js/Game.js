46d5dbf6a3210621c856618822c8154f0c470797
Dominic Meyer <dominic.meyer@me.com>
2020-03-28 13:59:45 +0100

    Game Class: adding the constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase("How are you"),
+            new Phrase("Dodo is a god"),
+            new Phrase("Stay the fuck home"),
+            new Phrase("May the force be with you"),
+            new Phrase("Stupid is as stupid does")
+        ];
+        this.activePhrase = null;
+    }
+}
